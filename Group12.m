f57875fec067b79f70ec0af62c2069a42432e0e4
whatamidoinglmao <76539111+whatamidoinglmao@users.noreply.github.com>
2022-11-19 18:10:18 -0800

    init commit

---
+++ Group12.m
@@ -0,0 +1,19 @@
+%==========================================================================
+% 
+%  BME 60B, Fangyuan Ding, MWF 4:00 - 4:50pm
+%  
+%  Sandbox Project
+%
+%  Group 12:
+%  Kentstar Samuel Harsono, 
+%  Eric Hyun Kim, 
+%  Tair Kuzhekov, 
+%  Elian Martin Sian Lintag
+% 
+%==========================================================================
+
+clear 
+close all
+clc
+
+
