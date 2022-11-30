f2b7b76248ad212f5ab62aa4cec046fb6730dbd2
whatamidoinglmao <76539111+whatamidoinglmao@users.noreply.github.com>
2022-11-29 23:05:02 -0800

    yay new stuff

--- Group12.m
+++ Group12.m
@@ -25,4 +25,10 @@ cols = 10;
 numMines = 20;
 
 % initialize game engine
-engine = mineEngine(rows, cols, numMines);
+engine = mineEngine(rows, cols, numMines);\
+
+
+
+% code code code
+
+disp("wow this some cool code yay :)")
