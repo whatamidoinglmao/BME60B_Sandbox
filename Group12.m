42a3afb0dcbc974e69bbc93af0175a8fd07a9ffd
Tair Kuzhekov <115375092+Tairo2004@users.noreply.github.com>
2022-12-02 17:59:25 -0800

    Added callback function
    Using gco I defined the button index that the user chose! So that the selected button will disappear. Gosh, finally it works

--- Group12.m
+++ Group12.m
@@ -15,7 +15,9 @@
 % 
 %==========================================================================
 
-clear 
+function GUI_buttons()
+
+clear
 close all
 clc
 
@@ -36,10 +38,23 @@ buttons = gobjects(rows, cols);
 
 for i = 1:rows
     for j = 1:cols
-        buttons(i,j) = uicontrol('Style','Pushbutton',...
-            'position',[10+j*34,10+i*34,35,35]);
+        buttons(i,j) = uicontrol('Style', 'pushbutton',...
+            'position',[10+j*34,10+i*34,35,35], 'Callback',@buttonPushed,...
+            'UserData', [i, j]);
     end
 end
 
 % open the figure
 set(graphics,'Visible','on')
+
+
+
+% callback function
+    function buttonPushed(src, event)
+        index = get(gco, 'UserData');
+        i = index(1);
+        j = index(2);
+        set(buttons(i, j), 'Visible','off');
+
+    end
+end
