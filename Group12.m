6a53c8b44b641b6e699bb9bf9e00cbae042a6e62
whatamidoinglmao <76539111+whatamidoinglmao@users.noreply.github.com>
2022-12-05 00:20:14 -0800

    all da graphics

--- Group12.m
+++ Group12.m
@@ -11,13 +11,21 @@
 %  Elian Martin Sian Lintag
 % 
 %  Description:
-% it's just minesweeper
+%  it's just minesweeper
 % 
 %==========================================================================
 
 clear
 close all
 clc
+
+fprintf(['welcome to...\n\n' ...      
+'        .__                                                    ._.           \n' ...
+'  _____ |__| ____   ____     ____   ____ ______   ___________  | |           \n' ...
+' /     \\|  |/    \\_/ __ \\  _/ __ \\_/ __ \\\\____ \\_/ __ \\_  __ \\ | |  \n' ...
+'|  Y Y  \\  |   |  \\  ___/  \\  ___/\\  ___/|  |_> >  ___/|  | \\/  \\|     \n' ...
+'|__|_|  /__|___|  /\\___  >  \\___  >\\___  >   __/ \\___  >__|     __       \n' ...
+'      \\/        \\/     \\/       \\/     \\/|__|        \\/         \\/  \n\n'])
 
 prompt ='Choose difficulty: custom grid = 0, easy = 1, intermediate = 2, hard = 3 (type 0,1, 2, or 3) \n';
 custom_prompt = 'Input how big you want the grid to be. ex: Inputting 3 would return 3 by 3 grid.\n';
@@ -28,21 +36,32 @@ while true
     if (User_output == 1)
         number = 5;
         mines = 4;
+        fprintf('easy? lol\n')
         break;
     elseif (User_output== 2)
         number = 10;
         mines = 15;
+        fprintf('ok nice\n')
         break;
     elseif (User_output == 3)
         number = 15;
         mines = 35;
+        fprintf('damn ok\n')
         break;
     elseif (User_output == 0)
-        number = input(custom_prompt);
-        mines = round(0.15 * number^2); 
+        while true
+            number = round(input(strcat('\n', custom_prompt)));
+            if number > 0
+                mines = round(0.15 * number^2); 
+                fprintf('nice\n')
+                break
+            else
+                fprintf('bro. please. it has to be above 0\n\n')
+            end
+        end
         break;
     else 
-        fprintf('Invalid answer. Choose 0,1,2, or 3.') 
+        fprintf('Invalid answer. Choose 0,1,2, or 3.\n\n') 
         User_output = input(prompt);
     end 
 end
--- mineEngine.m
+++ mineEngine.m
@@ -55,39 +55,97 @@ classdef mineEngine
             xoff = 10;
             yoff = 10;
             xSpace = 250;
-            ySpace = 100;
+            ySpace = 120;
+            figureWidth = xoff+(rows*spaceWidth)+xSpace;
+            figureHeight = yoff+(cols*spaceWidth)+ySpace;
 
             % init empty figure 10x10
-            obj.window = figure('Name','mine eeper',...
+            obj.window = figure('Name','mine eepy..',...
                 'NumberTitle','off',...
-                'Position', [500, 300, xoff+(rows*spaceWidth)+xSpace, yoff+(cols*spaceWidth)+ySpace], ...
+                'Position', [500, 300, figureWidth, figureHeight], ...
                 'Resize', 'off', ...
-                'Visible', 'off');
+                'Visible', 'off', ...
+                'Toolbar', 'None');
             
             %-------extra stuff in figure that should stay constant--------
 
             % number of extra elements in figure (UPDATE WHENEVER YOU ADD SOMETHING)
-            obj.xtraUI = 3;
+            obj.xtraUI = 13;
 
             % figure axes
             axes('Units', 'pixels', ...
                 'PlotBoxAspectRatio', [1,1,1], ...
                 'Position', [xoff+spaceWidth,yoff+spaceWidth, rows*spaceWidth, cols*spaceWidth], ... % if we change the position of the buttons this position has to match the first button
                 'XLim', [0, cols*35], ...
-                'YLim', [0, rows*35]);
+                'YLim', [0, rows*35], ...
+                'Box', 'on', ...
+                'Xtick', [], ...
+                'Ytick', [], ...
+                'Color', 1/255*[210,210,210], ...
+                'LineWidth', 0.5, ...
+                'XColor', 1/255*[180,180,180], ...
+                'YColor', 1/255*[180,180,180]);
+            disableDefaultInteractivity(gca)
 
             % new game button
             uicontrol('Style', 'Pushbutton', ...
-                'Position', [xoff+(rows*spaceWidth)+100, yoff+(cols/5)*spaceWidth, 100, 50], ...
+                'Position', [xoff+(rows*spaceWidth)+100, yoff+((cols+1)/2)*spaceWidth-100, 100, 50], ...
                 'Callback', @obj.newGame, ...
-                'String', "new game");
+                'String', 'new game :)', ...
+                'FontName', 'Comic Sans MS', ...
+                'FontSize', 10);
 
             % mine counter
             uicontrol('Style','Text', ...
-                'Position', [xoff+((rows/2)*spaceWidth), yoff+cols*spaceWidth+50, 50, 50], ...
-                'String', num2str(numMines));
+                'Position', [xoff+(rows*spaceWidth)+100, yoff+((cols+1)/2)*spaceWidth, 100, 30], ...
+                'String', num2str(numMines), ...
+                'FontName', 'Comic Sans MS', ...
+                'FontSize', 15, ...
+                'FontWeight', 'bold');
+
+            % "remaining:"
+            uicontrol('Style','Text', ...
+                'Position', [xoff+(rows*spaceWidth)+100, yoff+((cols+1)/2)*spaceWidth+30, 100, 30], ...
+                'String', 'remaining:', ...
+                'FontName', 'Comic Sans MS', ...
+                'FontSize', 13);
+
+            % mine eeper title
+            uicontrol('Style','Text', 'String', 'm', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)-166, figureHeight-56, 40, 40]);
+            uicontrol('Style','Text', 'String', 'i', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)-129, figureHeight-81, 40, 40]);
+            uicontrol('Style','Text', 'String', 'n', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)-96, figureHeight-62, 40, 40]);
+            uicontrol('Style','Text', 'String', 'e', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)-68, figureHeight-47, 40, 40]);
+            uicontrol('Style','Text', 'String', 'e', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)+10, figureHeight-62, 40, 40]);
+            uicontrol('Style','Text', 'String', 'e', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)+40, figureHeight-77, 40, 40]);
+            uicontrol('Style','Text', 'String', 'p', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)+73, figureHeight-76, 40, 40]);
+            uicontrol('Style','Text', 'String', 'e', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)+111, figureHeight-52, 40, 40]);
+            uicontrol('Style','Text', 'String', 'r', 'FontName', 'Comic Sans MS', 'FontSize', 18, ...
+                'Position', [(figureWidth/2)+140, figureHeight-40, 40, 40]);
 
             %--------------------------------------------------------------
+
+            % draw grid lines in axes
+            for i = 1:rows
+                line('XData',[0, cols*buttonWidth], ...
+                    'YData',[i*buttonWidth, i*buttonWidth],...
+                    'Color', 1/255*[180, 180, 180], ...
+                    'LineWidth', 0.25);
+            end
+            
+            for i = 1:cols
+                line('XData',[i*buttonWidth, i*buttonWidth], ...
+                    'YData',[0, rows*buttonWidth],...
+                    'Color', 1/255*[180, 180, 180], ...
+                    'LineWidth', 0.25);
+            end
 
             % init buttons for the game
             mineButtons = gobjects(rows, cols);
@@ -98,14 +156,16 @@ classdef mineEngine
                         'position',[xoff+j*spaceWidth,yoff+i*spaceWidth,buttonWidth,buttonWidth], ...
                         'Callback', @obj.buttonPressed, ...
                         'ButtonDownFcn', @obj.flagBomb, ...
-                        'UserData', [i, j, 0,0]);
+                        'UserData', [i, j, 0,0], ...
+                        'FontName', 'Comic Sans MS', 'FontSize', 20, ...
+                        'ForegroundColor', 1/255*[230,68,32]);
                 end
             end
 
         end
         
         % flagging a tile
-        function flagBomb(obj, src, evt)
+        function flagBomb(obj, src, ~)
 
             figHandle = ancestor(src, 'figure');
             click = get(figHandle, 'SelectionType');
@@ -128,20 +188,47 @@ classdef mineEngine
                 % if the current button isn't flagged
                 if flagStatus == 0
 
-                    set(src, 'Callback', '') % prevents being able to uncover tile
-                    set(src, 'String', 'F')
-                    set(src, 'UserData', [row,col,1,0]);
+                    set(src, 'Callback', '', ... % prevents being able to uncover tile
+                        'String', 'O', 'FontSize', 18, ...
+                        'UserData', [row,col,1,0], ...
+                        'BackgroundColor', 1/255*[200,200,200])
 
                     remaining = remaining - 1;
                     set(remainingText, 'String', num2str(remaining))
 
+                    % CHECK FOR A WIN
+                    if remaining == 0
+                        numUI = numel(figHandle.Children);
+                        mineButtonEnd = numUI - obj.xtraUI;
+                        buttonArray = figHandle.Children(1:mineButtonEnd);
+                        flagCells = get(buttonArray, 'UserData');
+                        flagMap = fliplr(rot90(reshape(flagCells,[height(obj.minefield),width(obj.minefield)]),1)); % Reformatted array button matrix
+
+                        % finding where player put flags
+                        flags = false(size(flagMap));
+                        for i = 1:obj.gameStats(1)
+                            for j = 1:obj.gameStats(1)
+                                data = cell2mat(flagMap(i,j));
+                                flags(i,j) = data(3);
+                            end
+                        end
+
+                        % comparing to actual minefield
+                        if flags == obj.minefield
+                            msgbox('WOOOOOO you won!', 'win!')
+                            buttonMap = fliplr(rot90(reshape(buttonArray,[height(obj.minefield),width(obj.minefield)]),1));
+                            set(buttonMap(flags), 'Callback', '', 'ButtonDownFcn', '') % disable mines
+                        end
+
+                    end
 
                 % if the current button is flagged
                 elseif flagStatus == 1
 
-                    set(src, 'Callback', @obj.buttonPressed)
-                    set(src, 'String', '')
-                    set(src, 'UserData', [row,col,0,0])
+                    set(src, 'Callback', @obj.buttonPressed, ...
+                        'String', '', 'FontSize', 20,...
+                        'UserData', [row,col,0,0], ...
+                        'BackgroundColor', 1/255*[240,240,240])
 
                     remaining = remaining + 1;
                     set(remainingText, 'String', num2str(remaining))
@@ -153,7 +240,7 @@ classdef mineEngine
         end
 
         % button press function
-        function buttonPressed(obj, src, evt)
+        function buttonPressed(obj, src, ~)
 
             buttData = get(src, 'UserData');
             row = buttData(1);
@@ -172,7 +259,10 @@ classdef mineEngine
             if check
 
                 mines = gridMap(obj.minefield);
-                set(mines, 'BackgroundColor', 'r', 'String', 'X');
+                set(mines, 'BackgroundColor', 'r', 'String', 'X', ...
+                    'ForegroundColor', 'k');
+
+                msgbox('u died lol', 'gameover', 'error')
 
                 % disable all the buttons
                 for i = 1:numButtons
@@ -206,7 +296,7 @@ classdef mineEngine
             end
 
             % Identifying numField == 0
-            if obj.numfield(row,col) == 0 && visited == 0
+            if num == 0 && visited == 0
 
                 set(gridMap(row,col), 'Visible', 'off');
                 set(gridMap(row,col), 'UserData', [row, col, 0,1]);
@@ -238,13 +328,34 @@ classdef mineEngine
                 catch
                 end
 
-            elseif obj.numfield(row,col)~=0 && visited == 0 % Closing recursion (non-zero in Numfield)
+            elseif num~=0 && visited == 0 % Closing recursion (non-zero in Numfield)
 
                 set(gridMap(row,col),'Visible', 'off');
 
                 % draws number when a nonzero tile is uncovered
-                text('Position', [((col-0.5)*35)-3, ((row-0.5)*35)+2], ...
-                    'String', num2str(obj.numfield(row, col)));
+                a = text('Position', [((col-0.5)*35)-5, ((row-0.5)*35)+1.5], ...
+                    'String', num2str(obj.numfield(row, col)), ...
+                    'FontName', 'Comic Sans MS', ...
+                    'FontSize', 14);
+
+                % set color based on number
+                if num == 1
+                    set(a, 'Color', 1/255*[14,60,200])
+                elseif num == 2
+                    set(a, 'Color', 1/255*[31, 145, 59])
+                elseif num == 3
+                    set(a, 'Color', 1/255*[212, 70, 70])
+                elseif num == 4
+                    set(a, 'Color', 1/255*[103, 87, 153])
+                elseif num == 5
+                    set(a, 'Color', 1/255*[143, 57, 20])
+                elseif num == 6
+                    set(a, 'Color', 1/255*[23, 191, 183])
+                elseif num == 7
+                    set(a, 'Color', 1/255*[64, 64, 64])
+                elseif num == 8
+                    set(a, 'Color', 1/255*[45,45,45])
+                end
 
                 set(gridMap(row,col), 'UserData', [row, col, 0, 1]);
 
@@ -252,8 +363,8 @@ classdef mineEngine
       
         end
 
-        % start a new game (experimental)
-        function newGame(obj,src,evt)
+        % start a new game without closing the figure
+        function newGame(obj,src,~)
 
             % delete everything from prev game
             figHandle = ancestor(src, 'figure');
@@ -291,6 +402,21 @@ classdef mineEngine
             % parameters for drawing figure
             buttonWidth = 35; spaceWidth = 34; xoff = 10; yoff = 10; % IMPORTANT ********** UPDATE THIS IF YOU UPDATE IT AT THE START
 
+            % draw lines again
+            for i = 1:rows
+                line('XData',[0, cols*buttonWidth], ...
+                    'YData',[i*buttonWidth, i*buttonWidth],...
+                    'Color', 1/255*[180, 180, 180], ...
+                    'LineWidth', 0.25);
+            end
+            
+            for i = 1:cols
+                line('XData',[i*buttonWidth, i*buttonWidth], ...
+                    'YData',[0, rows*buttonWidth],...
+                    'Color', 1/255*[180, 180, 180], ...
+                    'LineWidth', 0.25);
+            end
+
             remainingText = figHandle.Children(end - 2);
             set(remainingText, 'String', num2str(numMines))
 
@@ -303,7 +429,9 @@ classdef mineEngine
                         'position',[xoff+j*spaceWidth,yoff+i*spaceWidth,buttonWidth,buttonWidth], ...
                         'Callback', @obj.buttonPressed, ...
                         'ButtonDownFcn', @obj.flagBomb, ...
-                        'UserData', [i, j, 0,0]);
+                        'UserData', [i, j, 0,0], ...
+                        'FontName', 'Comic Sans MS', 'FontSize', 20, ...
+                        'ForegroundColor', 1/255*[230,68,32]);
                 end
             end
         end
